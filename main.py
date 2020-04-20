7c93c0fc115b16203304344e5311fe0ab64f3584
misantrhope13 <alexmoro112@mail.ru>
2020-04-20 19:14:39 +0300

    Допилил запуск сайта, так же сделал функцию !get_url только для 1 года и то не всё.

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 import threading
-from data.parametrs.get_data import get_result, list_1, list_2, list_3
+from data.parametrs.get_data import get_result, id
 import discord
 import datetime
 import asyncio
 import random
-from data.website.url import app
+from flask import Flask
+from flask import render_template
+from data.parametrs.get_data import get_result, list_words
 
 id_channel_chatting = 695558777805471764
 member_list = []
+global list_words
+get_result('data/parametrs/data.db')
+app = Flask(__name__)
+letters = []
+
+for i in range(len(list_words)):
+    letter = str(list_words[0][i]) + " " + list_words[1][i]
+    letters.append(letter)
+
+
+@app.route('/year/1')
+def year_1():
+    return render_template('index.html', letters=letters)
 
 
 # server_id = 695558777360875581
 
 def read_the_token():
-    with open('data\key\key.txt', 'r') as f:
+    with open('data/key/key.txt', 'r') as f:
         lines = f.readlines()
         return lines[0].strip()
 
@@ -49,6 +64,15 @@ async def on_message(message):
                 x = member_list[random.randint(0, int(count))]
                 print(x)
             await channel.send(f"""К доске пойдёт @{x}""")
+        elif message.content.startswith('!list'):
+            if message.content.find("!list 1") != -1:
+                await channel.send('http://127.0.0.1:8080/year/1')
+                app.run(port=8080, host='127.0.0.1')
+        elif message.content.startswith("!get_url"):
+            parametr = message.content[9:].split(" ")
+            if parametr[0] == '1':
+                await channel.send(
+                    list_words[1][int(parametr[1]) - 1] + " " + "-" + " " + list_words[2][int(parametr[1]) - 1])
 
     else:
         print("Юзер {0.author} написал: {0.content}".format(message))
@@ -60,7 +84,6 @@ async def time_message():
     channel = client.get_channel(id_channel_chatting)
     while not client.is_closed():
         try:
-
             time = str(datetime.datetime.now().time())[:5]
             date = str(datetime.datetime.now().weekday())
             if (time == "17:55" and date == '0') or (time == "17:55" and date == "4"):
@@ -91,18 +114,9 @@ async def update_members_list():
         await asyncio.sleep(3600)
 
 
-async def update_data():
-    await client.wait_until_ready()
-    try:
-        get_result("data\parametrs\data.db")
-    except Exception as e:
-        print(e)
-        await asyncio.sleep(120)
-
-
-client.loop.create_task(update_data())
 client.loop.create_task(update_members_list())
 client.loop.create_task(time_message())
+
 
 def start():
     client.run(token)
@@ -111,4 +125,3 @@ def start():
 if __name__ == '__main__':
     th = threading.Thread(target=start)
     th.start()
-    app.run(port=8080, host='127.0.0.1')
