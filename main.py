d70df28d63ec03668b3a5079c80f3ef11afbdf69
misantrhope13 <alexmoro112@mail.ru>
2020-04-23 11:39:12 +0300

    Пофиксил баги, сделал сайт для листа с 2 годом.

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
 import threading
-from data.parametrs.get_data import get_result, id
+
 import discord
 import datetime
 import asyncio
 import random
 from flask import Flask
 from flask import render_template
-from data.parametrs.get_data import get_result, list_words
+from data.parametrs.get_data import get_result, list_words_1, id_1, list_words_2, id_2
 
 id_channel_chatting = 695558777805471764
 member_list = []
-global list_words
+global list_words_1
 get_result('data/parametrs/data.db')
 app = Flask(__name__)
-letters = []
+letters_1 = []
 
-for i in range(len(list_words)):
-    letter = str(list_words[0][i]) + " " + list_words[1][i]
-    letters.append(letter)
+for i in range(len(id_1) // 3):
+    letter = str(list_words_1[0][i]) + ")" + " " + list_words_1[1][i]
+    letters_1.append(letter)
+
+letters_2 = []
+for i in range(len(id_2) // 3):
+    letter = str(list_words_2[0][i]) + ")" + " " + list_words_2[1][i]
+    letters_2.append(letter)
 
 
 @app.route('/year/1')
 def year_1():
-    return render_template('index.html', letters=letters)
+    return render_template('index.html', letters=letters_1)
+
+
+@app.route('/year/2')
+def year_2():
+    return render_template('index.html', letters=letters_2)
 
 
 # server_id = 695558777360875581
@@ -66,13 +76,16 @@ async def on_message(message):
             await channel.send(f"""К доске пойдёт @{x}""")
         elif message.content.startswith('!list'):
             if message.content.find("!list 1") != -1:
-                await channel.send('http://127.0.0.1:8080/year/1')
+                await message.channel.send('http://127.0.0.1:8080/year/1')
+                app.run(port=8080, host='127.0.0.1')
+            elif message.content.find("!list 2") != -1:
+                await message.channel.send('http://127.0.0.1:8080/year/2')
                 app.run(port=8080, host='127.0.0.1')
         elif message.content.startswith("!get_url"):
             parametr = message.content[9:].split(" ")
             if parametr[0] == '1':
                 await channel.send(
-                    list_words[1][int(parametr[1]) - 1] + " " + "-" + " " + list_words[2][int(parametr[1]) - 1])
+                    list_words_1[1][int(parametr[1]) - 1] + " " + "-" + " " + list_words_1[2][int(parametr[1]) - 1])
 
     else:
         print("Юзер {0.author} написал: {0.content}".format(message))
