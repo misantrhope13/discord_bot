fd00104f67513eeb39b8f0a461db68da98e184a9
misantrhope13 <alexmoro112@mail.ru>
2020-04-23 11:38:39 +0300

    Доделал базу данных, подогнал функцию к ней.

--- data/parametrs/get_data.py
+++ data/parametrs/get_data.py
@@ -1,10 +1,10 @@
 import sqlite3
 
-id = []
-list_words = [[], [], []]
+id_1 = []
+list_words_1 = [[], [], []]
 
-topics_1 = []
-url_1 = []
+id_2 = []
+list_words_2 = [[], [], []]
 
 
 def get_result(name):
@@ -14,16 +14,29 @@ def get_result(name):
 
     result_1 = cur.execute("""SELECT id,topic,url FROM year_1
  """).fetchall()
+    result_2 = cur.execute("""SELECT id,topic,url FROM year_2""").fetchall()
     for elem in result_1:
         for i in elem:
-            id.append(str(i))
+            id_1.append(str(i))
 
-    for i in id:
+    for i in id_1:
         if i.isdigit():
-            list_words[0].append(i)
+            list_words_1[0].append(i)
         elif 'https' in i:
-            list_words[2].append(i)
+            list_words_1[2].append(i)
         else:
-            list_words[1].append(i)
+            list_words_1[1].append(i)
+
+    for elem in result_2:
+        for i in elem:
+            id_2.append(str(i))
+
+    for i in id_2:
+        if i.isdigit():
+            list_words_2[0].append(i)
+        elif 'https' in i:
+            list_words_2[2].append(i)
+        else:
+            list_words_2[1].append(i)
 
     con.close()
